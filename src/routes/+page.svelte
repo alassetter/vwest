d56b38fbd71dc09a22b2eedc0b6091eba5310db1
Andrew <alassetter@skyymedia.com>
2023-10-23 15:39:33 -0500

    SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,53 @@
+<script>
+  import SvelteSEO from "$lib";
+  import SEO from "./";
+</script>
+
+<SvelteSEO
+  title={SEO.title}
+  description={SEO.description}
+  keywords={SEO.keywords.toString()}
+  base={SEO.base}
+  applicationName={SEO.applicationName}
+  themeColor={SEO.themeColor}
+  nositelinkssearchbox={SEO.nositelinkssearchbox}
+  notranslate={SEO.notranslate}
+  canonical={SEO.canonical}
+  amp={SEO.amp}
+  manifest={SEO.manifest}
+  languageAlternates={SEO.languageAlternates}
+  twitter={SEO.twitter}
+  openGraph={SEO.openGraph}
+  jsonLd={{
+    "@type": "Article",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": "https://example.com/article",
+    },
+    headline: "Article headline",
+    image: [
+      "https://example.com/photos/1x1/photo.jpg",
+      "https://example.com/photos/4x3/photo.jpg",
+      "https://example.com/photos/16x9/photo.jpg",
+    ],
+    datePublished: "2020-08-03T17:31:37Z",
+    dateModified: "2020-08-20T09:31:37Z",
+    author: {
+      "@type": "Person",
+      name: "John Doe",
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "Google",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://example.com/logo.jpg",
+      },
+    },
+  }}
+/>
+
+
 <section class="py-24 md:py-24 bg-coolGray-50">
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap -mx-4 mb-16">
