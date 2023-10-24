fea5a2f210ba5f235771446951ffe636b149a977
Andrew <alassetter@skyymedia.com>
2023-10-24 03:52:46 -0500

    Error Page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from '$app/stores';
+	import '../app.postcss';
+</script>
+
+<div class="error">
+	<h2>{$page.status}</h2>
+	<p class="subhead">{$page.error.message}</p>
+
+	<p><strong>Sorry!</strong> Maybe try one of these links?</p>
+	<ul>
+		<li><a href="/">Home</a></li>
+	</ul>
+</div>
+
+
+<style>
+	.error {
+		height: 100%;
+		display: grid;
+		place-content: center;
+	}
+</style>
