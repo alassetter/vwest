18047d277e5e0c5f9255b56a85e0c541ec351cfd
Andrew <alassetter@skyymedia.com>
2023-10-24 05:58:34 -0500

    Add Navigation and Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+	import Header from '$lib/components/layout/Header.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
+</script>
+
+<Header />
+<slot />
+<Footer />
