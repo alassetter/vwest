5c94947bb8408f50ee79bd2c81d7f75778ecf8bd
Andrew <alassetter@skyymedia.com>
2023-10-24 16:26:32 -0500

    Navigation

--- src/lib/components/layout/NavItem.svelte
+++ src/lib/components/layout/NavItem.svelte
@@ -1,8 +1,16 @@
+<script>
+import { currentPage } from '$lib/assets/js/store'
+export let href
+$: isCurrentPage = $currentPage.startsWith(href)
+</script>
+
 <li>
   <a
     class="relative inline-block after:absolute after:bg-blue-300 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:translate-y-1 after:opacity-0 hover:after:translate-y-0 hover:after:opacity-100 after:transition after:ease-in-out after:duration-200 mr-4"
-    href="/"
-  >
-    Menu Item
+		href={href}
+		class:active={isCurrentPage}
+		aria-current={isCurrentPage ? 'page' : false}
+	>
+		<slot />
   </a>
 </li>
--- src/lib/components/layout/Navigation.svelte
+++ src/lib/components/layout/Navigation.svelte
@@ -1,14 +1,21 @@
 <script>
-	const vlogo = '/V-logo.svg';
+  import { navItems } from '$lib/config'
 	import NavItem from '$lib/components/layout/NavItem.svelte';
+  const vlogo = '/V-logo.svg';
+
+
 </script>
 
-<nav class="">
+<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
 	<a class="" href="/">
 		<span class="sr-only">Vortex West</span>
 		<img class="h-6 w-auto" src={vlogo} alt="Vortex West Logo" />
 	</a>
-	<ul class="">
-		<NavItem />
+	<ul class="hidden lg:flex lg:gap-x-12">
+    {#each navItems as page}
+		<NavItem href={page.route}>
+			{page.title}
+		</NavItem>
+		{/each}
 	</ul>
 </nav>
