61bbb725984a4d99c101f76d700d67ceb1d99cef
Andrew <alassetter@skyymedia.com>
2023-10-26 11:26:08 -0500

    Page Templates

---
+++ src/routes/article/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	const placeholder = '/images/placeholder-lg.jpg';
+</script>
+
+
+<article class="py-16 md:py-24 bg-white">
+  <div class="container px-4 mx-auto">
+    <div class="md:max-w-2xl mx-auto mb-12 text-center">
+      <div class="flex items-center justify-center">
+        <p class="inline-block text-blue-500 font-medium">John Doe</p>
+        <span class="mx-1 text-blue-500">&bull;</span>
+        <p class="inline-block text-blue-500 font-medium">19 Jan 2022</p>
+      </div>
+      <h2 class="mb-4 text-3xl md:text-5xl leading-tight text-darkgray-900 font-bold tracking-tight">Lorem ipsum dolor sit amet</h2>
+      <p class="mb-6 text-lg md:text-xl font-spaceg font-light text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div class="inline-block py-1 px-3 text-xs leading-5 text-blue-500 font-medium uppercase bg-blue-100 rounded-full shadow-sm">Technology</div>
+    </div>
+    <div class="mb-10 mx-auto max-w-max overflow-hidden rounded-lg">
+      <img src={placeholder} alt="">
+    </div>
+    <div class="md:max-w-3xl mx-auto">
+      <p class="mb-8 pb-10 text-lg md:text-xl font-thin text-gray-500 border-b border-gray-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo massa. Eu dolor aliquet risus gravida nunc at feugiat consequat purus. Non massa enim vitae duis mattis. Vel in ultricies vel fringilla.</p>
+      <h3 class="mb-4 text-2xl md:text-3xl font-semibold text-gray-800">Header 1</h3>
+      <p class="mb-4 text-base md:text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo massa. Eu dolor aliquet risus gravida nunc at feugiat consequat purus. Non massa enim vitae duis mattis. Vel in ultricies vel fringilla.</p>
+      <div class="mb-6 p-6 border-l-2 border-blue-500">
+        <p class="mb-4 text-xl md:text-2xl leading-tight font-medium text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo massa. Eu dolor aliquet risus gravida nunc at feugiat consequat purus.</p>
+        <span class="text-base md:text-lg text-gray-400 font-medium">&mdash; John Doe, CEO &amp; Founder</span>
+      </div>
+      <p class="mb-6 text-base md:text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo massa. Eu dolor aliquet risus gravida nunc at feugiat consequat purus. Non massa enim vitae duis mattis. Vel in ultricies vel fringilla.</p>
+      <div class="mb-4 max-w-max overflow-hidden rounded-md">
+        <img src={placeholder} alt="">
+      </div>
+      <p class="mb-8 text-base md:text-lg text-gray-400 font-medium">
+        <span>Non massa enim vitae duis mattis. Vel in</span>
+        <a class="hover:text-gray-600 underline" href="/">ultricies</a>
+        <span>vel fringilla.</span>
+      </p>
+      <h3 class="mb-4 text-2xl md:text-3xl font-semibold text-gray-800">Header 1</h3>
+      <p class="mb-14 text-base md:text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo massa. Eu dolor aliquet risus gravida nunc at feugiat consequat purus. Non massa enim vitae duis mattis. Vel in ultricies vel fringilla.</p>
+      <p class="mb-4 text-base md:text-lg text-gray-500">Sagittis et eu at elementum, quis in. Proin praesent volutpat egestas sociis sit lorem nunc nunc sit. Eget diam curabitur mi ac. Auctor rutrum lacus malesuada massa ornare et. Vulputate consectetur ac ultrices at diam dui eget fringilla tincidunt. Arcu sit dignissim massa erat cursus vulputate gravida id. Sed quis auctor vulputate hac elementum gravida cursus dis.</p>
+      <ol class="list-decimal list-inside md:px-5 mb-14 text-base md:text-lg text-gray-500">
+        <li>Lectus id duis vitae porttitor enim gravida morbi.</li>
+        <li>Eu turpis posuere semper feugiat volutpat elit, ultrices suspendisse. Auctor vel in vitae placerat.</li>
+        <li>Suspendisse maecenas ac donec scelerisque diam sed est duis purus.</li>
+      </ol>
+      <p class="mb-10 pb-10 text-base md:text-lg text-gray-500 border-b border-gray-100">
+        <span>Vestibulum placerat magna nulla, sit amet venenatis sapien consequat ut. Cras pulvinar, lorem tristique pharetra finibus, dui erat finibus orci, a vehicula arcu sapien id metus. Quisque quis lorem a sem porttitor feugiat. Etiam quis congue est. Donec fermentum ac libero a pretium. Nulla nisl sem, euismod ut eros vitae, egestas scelerisque enim.</span>
+        <a class="text-blue-500 hover:text-blue-600 underline" href="/">Vivamus id pharetra massa</a>
+        <span>. Nulla aliquet erat elit, a gravida dui efficitur vel. Sed pulvinar diam sed neque ullamcorper semper. Nulla elementum arcu lacus, quis porta nisl posuere varius. Quisque tempus libero sed urna posuere hendrerit.</span>
+      </p>
+    </div>
+  </div>
+</article>
--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,2 +1,12 @@
-
-<h1>careers</h1>
+<section class="py-12 md:py-24 bg-coolGray-50">
+  <div class="container mx-auto px-4">
+    <div class="flex flex-wrap -mx-4 mb-16">
+      <div class="lg:w-6/12 px-4 mb-12 lg:mb-0">
+        <h1 class="font-heading text-6xl md:text-10xl mb-8 tracking-tightest">Careers</h1>
+        <p class="text-2xl tracking-tight">
+          Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
