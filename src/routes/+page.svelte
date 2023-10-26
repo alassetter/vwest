559e64aefef42932bf4fc42fbef4153d358e68c3
Andrew <alassetter@skyymedia.com>
2023-10-26 11:27:12 -0500

    Adjustment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
+<script>
+import Hero from '$lib/components/content/Hero.svelte';
+</script>
+
+
+<Hero />
+
 <section class="py-24 md:py-24 bg-coolGray-50">
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap -mx-4 mb-16">
       <div class="lg:w-8/12 px-4 mb-12 lg:mb-0">
-        <h1 class="font-heading text-6xl md:text-10xl mb-8 tracking-tightest">Home</h1>
+        <h5 class="font-heading text-base md:text-lg mb-8 uppercase tracking-wider">Subheader</h5>
+        <h1 class="font-heading font-normal text-6xl md:text-10xl mb-8 tracking-tightest">Home</h1>
         <p class="text-2xl tracking-tight">
           Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         </p>
