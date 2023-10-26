450c073404f8d9ba834633dfef2a122814587279
Andrew <alassetter@skyymedia.com>
2023-10-26 11:23:41 -0500

    PostCss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,9 +6,7 @@ const config = {
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
-		autoprefixer({
-			cascade: true
-		})
+		autoprefixer
 	]
 };
 
