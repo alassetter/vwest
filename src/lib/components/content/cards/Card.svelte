2e0de6dad3371802c540b94b17d2b5d6f3627e4b
Andrew <alassetter@skyymedia.com>
2023-10-29 16:08:06 -0500

    Card Component

---
+++ src/lib/components/content/cards/Card.svelte
@@ -0,0 +1,18 @@
+<script>
+	const placeholder = '/images/placeholder.svg';
+</script>
+
+<div class="border bg-card text-card-foreground rounded-lg overflow-hidden shadow-lg max-w-sm mx-auto">
+  <img
+    src={placeholder}
+    alt="name"
+    width="320"
+    height="320"
+    class="object-cover w-full aspect-square"
+  />
+  <div class="p-4">
+    <h2 class="text-2xl font-bold">Title</h2>
+    <h3 class="text-gray-500">Subtitle</h3>
+    <p class="mt-2 text-gray-600">Maecenas faucibus mollis interdum.</p>
+  </div>
+</div>
