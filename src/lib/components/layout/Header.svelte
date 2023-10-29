4cd73a2bd4b643f23fd7659358970c3a85954dc2
Andrew <alassetter@skyymedia.com>
2023-10-29 09:25:31 -0500

    Fix header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Navigation from '$lib/components/layout/Navigation.svelte'
+  import Navigation from '$lib/components/layout/NavigationDark.svelte'
 </script>
 
-<header class="sticky top-0 z-50 bg-white shadow-md shadow-blue-900/5 transition duration-500">
+<header class="sticky top-10 z-50 transition duration-500">
 	<Navigation />
 </header>
