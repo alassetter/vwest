765b265ef87da2987ca71fabd56596c920cbaed8
Andrew <alassetter@skyymedia.com>
2023-10-29 17:21:44 -0500

    Hero Text Block Component
    Starting point for this

---
+++ src/lib/components/HeroTextBlock.svelte
@@ -0,0 +1,24 @@
+<!-- HeroTextBlock.svelte -->
+<script>
+  export let subhead = 'A Sub Title';
+  export let title = 'Title';
+  export let bodyCopy = 'paragraph copy';
+</script>
+
+<div class="container--main pt-16 pb-16">
+  <div class="flex flex-wrap">
+    <!-- Content -->
+    <div class="w-full md:8/12 lg:w-9/12 xl:w-7/12 2xl:6/12">
+      <p class="mb-8 font-heading text-base md:text-lg font-medium leading-none uppercase tracking-widest">
+        {subhead}
+      </p>
+      <h1 class="mb-8 font-heading text-6xl md:text-10xl tracking-tighter">
+        {title}
+      </h1>
+      <p class="text-2xl tracking-tight">
+        {bodyCopy}
+      </p>
+    </div>
+    <!-- /Content -->
+  </div>
+</div>
--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,12 +1,17 @@
-<section class="py-12 md:py-24 bg-coolGray-50">
-  <div class="container mx-auto px-4">
-    <div class="flex flex-wrap -mx-4 mb-16">
-      <div class="lg:w-6/12 px-4 mb-12 lg:mb-0">
-        <h1 class="font-heading text-6xl md:text-10xl mb-8 tracking-tightest">Privacy</h1>
-        <p class="text-2xl tracking-tight">
-          Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-        </p>
-      </div>
-    </div>
-  </div>
+<!-- Privacy Policy Page.svelte -->
+<script>
+  import HeroTextBlock from '$lib/components/HeroTextBlock.svelte';
+
+  let customSubhead = 'Privacy Policy';
+  let customTitle = 'We take your privacy seriously';
+  let customBodyCopy = 'Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.';
+</script>
+
+<section>
+  <!-- Include MyComponent and pass custom values as props -->
+  <HeroTextBlock
+    subhead={customSubhead}
+    title={customTitle}
+    bodyCopy={customBodyCopy}
+  />
 </section>
