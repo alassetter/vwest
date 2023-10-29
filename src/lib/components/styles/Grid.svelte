aacb5fef56e1f63d0d1bc14d70a7ad54178985e5
Andrew <alassetter@skyymedia.com>
2023-10-29 17:22:16 -0500

    Grid Docs

---
+++ src/lib/components/styles/Grid.svelte
@@ -0,0 +1,37 @@
+
+<div class="w-full m-w-full px-4 mx-auto">
+  <div class="flex flex-wrap">
+    <div class="w-full px-4 flex-1">
+        <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 1
+      </span>
+    </div>
+    <div class="w-full px-4 flex-1">
+       <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 2
+      </span>
+    </div>
+    <div class="w-full px-4 flex-1">
+       <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 3
+      </span>
+    </div>
+    <div class="w-full px-4 flex-1">
+       <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 4
+      </span>
+    </div>
+  </div>
+  <div class="flex flex-wrap">
+    <div class="w-8/12 px-4">
+       <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 8
+      </span>
+    </div>
+    <div class="w-4/12 px-4">
+       <span class="text-sm text-white block my-5 p-6 rounded border border-solid bg-blue-300 border-blue-600">
+        Column of 4
+      </span>
+    </div>
+  </div>
+</div>
