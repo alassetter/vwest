3515e43e3e0b545d92d6b05a1fb355a9d78f469a
Andrew <alassetter@skyymedia.com>
2023-10-31 11:04:21 -0500

    Pull over team section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	const placeholder = '/images/placeholder-lg.jpg';
+	const headshot = '/images/headshot.jpg';
 	import HeroSolid from '$lib/components/content/HeroSolid.svelte';
 </script>
 
@@ -7,36 +9,246 @@
 	subheadColor="text-gray-300"
 	title="About"
 	titleColor="text-white"
-	bodyCopy=" sdfsd"
+	bodyCopy=" "
 	bodyColor="text-white"
 	backgroundColor="bg-blue-500"
 	gradient=""
 />
 
-<section class="bg-gray-300 text-black">
-	<div class="pt-16 pb-16">
+<section class="bg-gray-10">
+	<div class="pt-24 pb-24">
 		<!-- Global Padding -->
 		<div class="px-12">
 			<div class="w-full max-w-full mx-auto">
-				<div class="flex flex-wrap">
-					<!-- Content -->
-					<div class="w-full md:8/12 lg:w-9/12 xl:w-7/12 2xl:6/12">
-						<p class="mb-8 text-base font-medium uppercase">
-							Subheadline
-						</p>
-						<h1 class="mb-8 text-5xl max-w-2xl">
-							Aenean lacinia bibendum nulla sed consectetur.
+				<!-- Content -->
+				<div class="flex flex-wrap -mx-4 mb-16">
+					<div class="lg:w-8/12 px-4 mb-12 lg:mb-0">
+						<!-- <h5
+							class="font-heading text-subhead mb-8 font-medium uppercase"
+						>
+							About us
+						</h5> -->
+						<h1 class="font-heading text-5xl mb-8 tracking-tightest">
+							Established Expertise In Inovative Software Solutions
 						</h1>
-						<p class="text-lg">
-							Vestibulum id ligula porta felis euismod semper.
-							Maecenas faucibus mollis interdum. Aenean lacinia
-							bibendum nulla sed consectetur. Duis mollis, est non
-							commodo luctus, nisi erat porttitor ligula, eget lacinia
-							odio sem nec elit. Cras justo odio, dapibus ac facilisis
-							in, egestas eget quam.
+						<p class="text-xl tracking-tight">
+							<span class="font-heading text-gray-600"
+								>Vortex West is a boutique product and software
+								development agency, headquartered in the United
+								States, with an additional office in Belgrade, Serbia.</span
+							>
 						</p>
 					</div>
-					<!-- Content -->
+					<div class="lg:w-4/12 px-4 text-right lg:mt-8" />
+				</div>
+				<div class="flex flex-wrap -mx-4">
+					<div class="w-full lg:w-7/12 xl:w-7/12 px-4 mb-20 xl:mb-0">
+						<div class="flex flex-wrap -mx-4 align-items-stretch">
+							<div class="w-full md:w-1/2 px-2 mb-4 md:mb-0">
+								<div
+									class="flex items-end mb-4 pl-8 pr-1 xl:pl-10 h-64 pb-10 border rounded-xl"
+								>
+									<div>
+										<h5 class="font-heading text-8xl">4</h5>
+										<span class="block text-gray-600"
+											>Years In Business</span
+										>
+									</div>
+								</div>
+								<div
+									class="flex items-end mb-4 pl-8 pr-1 xl:pl-10 h-64 pb-10 border rounded-xl bg-white shadow-sm"
+								>
+									<div>
+										<h5 class="font-heading text-8xl">25+</h5>
+										<span class="block text-gray-600"
+											>Full Time Engineers</span
+										>
+									</div>
+								</div>
+							</div>
+							<div class="w-full md:w-1/2 px-2">
+								<div
+									class="flex items-end mb-4 pl-8 pr-1 xl:pl-10 h-64 pb-10 border rounded-xl bg-white shadow-sm"
+								>
+									<div>
+										<h5 class="font-heading text-7xl text-blue-500">
+											0%
+										</h5>
+										<span class="block text-gray-600"
+											>Staff Turnover</span
+										>
+									</div>
+								</div>
+								<div
+									class="flex items-end mb-4 pl-8 pr-1 xl:pl-10 h-64 pb-10 border rounded-xl bg-white shadow-sm"
+								>
+									<div>
+										<h5 class="font-heading text-7xl text-blue-500">
+											125+
+										</h5>
+										<span class="block text-gray-700 font-medium"
+											>Years of Experience</span
+										>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="w-full lg:w-5/12 lg:pl-16 xl:w-5/12 2xl:5/12 px-4"
+					>
+						<p class="tracking-tight text-lg">
+							Our core focus lies in fostering business growth by
+							delivering innovative software solutions and providing
+							forward-thinking companies access to the finest
+							engineering talent in Southeastern Europe. Passion is
+							the driving force behind our work, as we are committed
+							to create solutions that propel our clients' businesses
+							to new heights.
+						</p>
+					</div>
+				</div>
+				<!-- / Content -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="bg-gray-100">
+	<div class="pt-24 pb-24">
+		<!-- Global Padding -->
+		<div class="px-12">
+			<div class="flex flex-wrap -mx-4 items-center mb-20">
+				<div class="w-full lg:w-8/12 xl:w-1/2 px-4 mb-8 lg:mb-0">
+					<h1 class="font-heading text-6xl">Meet the team</h1>
+				</div>
+				<div class="w-full lg:w-4/12 xl:w-1/2 px-4">
+					<div class="flex items-center justify-end">
+						<!-- Left Button -->
+						<a
+							class="inline-flex h-16 sm:h-18 w-16 sm:w-18 mr-8 items-center justify-center text-black hover:text-white hover:bg-black border border-black rounded-full transition duration-200"
+							href="/"
+						>
+							<svg
+								width="27"
+								height="27"
+								viewBox="0 0 27 27"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M10.7051 7.12817L4.15732 13.6759L10.7051 20.2237"
+									stroke="currentColor"
+									stroke-width="1.61806"
+									stroke-miterlimit="10"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M22.4941 13.6759H4.33949"
+									stroke="currentColor"
+									stroke-width="1.61806"
+									stroke-miterlimit="10"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</a>
+						<!-- Right Button -->
+						<a
+							class="inline-flex h-16 sm:h-18 w-16 sm:w-18 items-center justify-center text-black hover:text-white hover:bg-black border border-black rounded-full transition duration-200"
+							href="/"
+						>
+							<svg
+								width="27"
+								height="27"
+								viewBox="0 0 27 27"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M16.2949 7.12817L22.8427 13.6759L16.2949 20.2237"
+									stroke="currentColor"
+									stroke-width="1.61806"
+									stroke-miterlimit="10"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M4.50586 13.6759H22.6605"
+									stroke="currentColor"
+									stroke-width="1.61806"
+									stroke-miterlimit="10"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="flex items-center">
+				<div class="flex-shrink-0 mr-8 w-full max-w-md">
+					<img
+						class="block w-full mb-8 border rounded-2xl overflow-hidden shadow-sm"
+						src={headshot}
+						alt="name"
+					/>
+					<div class="max-w-sm">
+						<h4 class="text-2xl font-medium">Richard Danicic</h4>
+						<span
+							class="mt-1 text-sm text-gray-600 uppercase font-medium font-spaceg"
+							>Chief Executive Officer</span
+						>
+					</div>
+				</div>
+				<div class="flex-shrink-0 mr-8 w-full max-w-md">
+					<img
+						class="block w-full mb-8 border rounded-2xl overflow-hidden shadow-sm"
+						src={headshot}
+						alt="name"
+					/>
+					<div class="max-w-sm">
+						<h4 class="text-2xl font-spaceg font-medium">
+							Nebojsa Knezevic
+						</h4>
+						<span
+							class="mt-1 text-sm text-blue-500 uppercase font-medium font-spaceg"
+							>Chief Technical Officer</span
+						>
+					</div>
+				</div>
+				<div class="flex-shrink-0 mr-8 w-full max-w-md">
+					<img
+						class="block w-full mb-8 border rounded-2xl overflow-hidden shadow-sm"
+						src={headshot}
+						alt="name"
+					/>
+					<div class="max-w-sm">
+						<h4 class="text-2xl font-spaceg font-medium">
+							Filip Micic
+						</h4>
+						<span
+							class="mt-1 text-sm text-gray-600 uppercase font-medium font-spaceg"
+							>Head of Engineering</span
+						>
+					</div>
+				</div>
+				<div class="flex-shrink-0 mr-8 w-full max-w-md">
+					<img
+						class="block w-full mb-8 border rounded-2xl overflow-hidden shadow-sm"
+						src={headshot}
+						alt="name"
+					/>
+					<div class="max-w-sm">
+						<h4 class="text-2xl font-spaceg font-medium">
+							Jovana Lujic
+						</h4>
+						<span
+							class="mt-1 text-sm text-gray-600 uppercase font-medium font-spaceg"
+							>Office & HR Operations Manager</span
+						>
+					</div>
 				</div>
 			</div>
 		</div>
