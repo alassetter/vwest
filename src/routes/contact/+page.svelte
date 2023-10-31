cb2f1c2d24b571b1b44a3ed487f234400dca1917
Andrew <alassetter@skyymedia.com>
2023-10-31 09:22:19 -0500

    Contact page
    add form. style inputs later

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -13,30 +13,166 @@
 	gradient=""
 />
 
-<section class="bg-gray-300 text-black">
+<section class="bg-gray-100">
 	<div class="pt-16 pb-16">
 		<!-- Global Padding -->
-		<div class="px-12">
-			<div class="w-full max-w-full mx-auto">
-				<div class="flex flex-wrap">
-					<!-- Content -->
-					<div class="w-full md:8/12 lg:w-9/12 xl:w-7/12 2xl:6/12">
-						<p class="mb-8 text-base font-medium uppercase">
-							Subheadline
-						</p>
-						<h1 class="mb-8 text-5xl max-w-2xl">
-							Aenean lacinia bibendum nulla sed consectetur.
-						</h1>
-						<p class="text-lg">
-							Vestibulum id ligula porta felis euismod semper.
-							Maecenas faucibus mollis interdum. Aenean lacinia
-							bibendum nulla sed consectetur. Duis mollis, est non
-							commodo luctus, nisi erat porttitor ligula, eget lacinia
-							odio sem nec elit. Cras justo odio, dapibus ac facilisis
-							in, egestas eget quam.
-						</p>
+		<div class="px-14">
+			<div class="flex flex-wrap -mx-4">
+				<div class="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+					<div class="max-w-xl mx-auto lg:mx-0">
+						<div class="max-w-lg mb-10">
+							<h1 class="font-heading text-5xl mb-8">Let`s talk</h1>
+							<p class="text-xl text-gray-600">
+								Donec id elit non mi porta gravida at eget metus. Cum
+								sociis natoque penatibus et magnis dis parturient
+								montes, nascetur ridiculus mus. Donec sed odio dui.
+							</p>
+						</div>
+						<div class="flex flex-wrap -mx-4 mb-20">
+							<div class="w-full sm:w-1/2 px-4 mb-6 sm:mb-0">
+								<div
+									class="py-6 px-6 text-left bg-white border border-gray-300 rounded-xl shadow-sm"
+								>
+									<span
+										class="block font-spaceg font-medium text-sm text-gray-600"
+										>Call Us</span
+									>
+									<span class="font-spaceg text-blue-500"
+										>214-000-0000</span
+									>
+								</div>
+							</div>
+							<div class="w-full sm:w-1/2 px-4">
+								<div
+									class="py-6 px-6 text-left bg-white border border-gray-300 rounded-xl shadow-sm"
+								>
+									<span
+										class="block font-spaceg font-medium text-sm text-gray-600"
+										>Email us</span
+									>
+									<span class="font-medium text-blue-500"
+										>contact@vortexwe.com</span
+									>
+								</div>
+							</div>
+						</div>
+						<div class="mb-16">
+							<span class="block mb-5 font-spaceg font-medium text-lg"
+								>Follow Us Online</span
+							>
+
+							<div class="flex flex-wrap items-center">
+								<!-- Twitter -->
+								<a class="inline-flex mr-8 items-center" href="/">
+									<svg
+										width="17"
+										height="13"
+										viewBox="0 0 17 13"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M16.0828 1.7882C15.5128 2.0352 14.9238 2.2062 14.2968 2.2822C14.9428 1.9022 15.4368 1.2942 15.6648 0.553195C15.0568 0.914195 14.3918 1.1612 13.6888 1.3132C13.1188 0.705195 12.3018 0.325195 11.4088 0.325195C9.67981 0.325195 8.29281 1.7312 8.29281 3.4412C8.29281 3.6882 8.31181 3.91619 8.36881 4.1442C5.78481 4.03019 3.50481 2.7762 1.96581 0.895195C0.863813 2.8712 2.09881 4.5052 2.91581 5.0562C2.42181 5.0562 1.92781 4.9042 1.50981 4.6762C1.50981 6.2152 2.59281 7.4882 3.99881 7.7732C3.69481 7.8682 3.01081 7.9252 2.59281 7.8302C2.99181 9.0652 4.15081 9.9772 5.49981 9.9962C4.43581 10.8322 2.87781 11.4972 0.882812 11.2882C2.26981 12.1812 3.90381 12.6942 5.67081 12.6942C11.4088 12.6942 14.5248 7.9442 14.5248 3.8402C14.5248 3.7072 14.5248 3.5742 14.5058 3.4412C15.1518 2.9662 15.6838 2.4152 16.0828 1.7882Z"
+											fill="black"
+										/>
+									</svg>
+									<span class="ml-3">Twitter</span>
+								</a>
+								<!-- LinkedIn -->
+								<a class="inline-flex items-center" href="/">
+									<svg
+										width="19"
+										height="19"
+										viewBox="0 0 19 19"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M16.0984 16.0993V11.3016C16.0984 8.94368 15.5908 7.14249 12.8399 7.14249C11.5136 7.14249 10.6293 7.86297 10.2691 8.55069H10.2364V7.35536H7.63281V16.0993H10.351V11.7601C10.351 10.6139 10.5639 9.51679 11.9721 9.51679C13.3639 9.51679 13.3803 10.8104 13.3803 11.8256V16.083H16.0984V16.0993Z"
+											fill="black"
+										/>
+										<path
+											d="M3.21094 7.35538H5.92911V16.0994H3.21094V7.35538Z"
+											fill="black"
+										/>
+										<path
+											d="M4.57195 2.99991C3.7041 2.99991 3 3.70401 3 4.57186C3 5.43971 3.7041 6.16019 4.57195 6.16019C5.4398 6.16019 6.14391 5.43971 6.14391 4.57186C6.14391 3.70401 5.4398 2.99991 4.57195 2.99991Z"
+											fill="black"
+										/>
+									</svg>
+									<span class="ml-3">Linkedin</span>
+								</a>
+							</div>
+						</div>
 					</div>
-					<!-- Content -->
+				</div>
+				<div class="w-full lg:w-1/2 px-4">
+					<div
+						class="py-12 px-8 md:px-10 bg-white rounded-4xl max-w-xl mx-auto lg:mr-0"
+					>
+						<h3 class="text-2xl font-spaceg mb-8">Contact Us</h3>
+						<form action="">
+							<input
+								class="block w-full py-4 px-6 h-14 mb-6 text-gray-500 bg-white border border-gray-400 rounded-full"
+								type="text"
+								placeholder="First Name*"
+							/>
+							<input
+								class="block w-full py-4 px-6 h-14 mb-6 text-gray-500 bg-white border border-gray-400 rounded-full"
+								type="text"
+								placeholder="Last Name*"
+							/>
+							<input
+								class="block w-full py-4 px-6 h-14 mb-6 text-gray-500 bg-white border border-gray-400 rounded-full"
+								type="email"
+								placeholder="E-mail"
+							/>
+							<input
+								class="block w-full py-4 px-6 h-14 mb-6 text-gray-500 bg-white border border-gray-400 rounded-full"
+								type="text"
+								placeholder="Phone number"
+							/>
+							<div class="relative">
+								<span
+									class="absolute top-1/2 right-0 mr-10 transform -translate-y-1/2"
+								>
+									<svg
+										width="12"
+										height="8"
+										viewBox="0 0 12 8"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M1.33203 1.66439L5.9987 6.33105L10.6654 1.66439"
+											stroke="black"
+											stroke-width="1.5"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										/>
+									</svg>
+								</span>
+								<select
+									class="relative block w-full py-4 px-8 h-14 mb-6 text-gray-500 bg-transparent border border-gray-400 rounded-full appearance-none outline-none"
+								>
+									<option selected=""
+										>What can we help you with?</option
+									>
+									<option>Software Development</option>
+									<option>General Inquiry</option>
+								</select>
+							</div>
+							<textarea
+								class="w-full h-40 py-4 px-6 text-gray-500 bg-white border border-gray-400 rounded-2xl resize-none mb-8"
+								placeholder="Message"
+								rows="3"
+							/>
+							<button
+								class="block w-full py-4 px-6 text-center font-medium text-white leading-none bg-black hover:bg-blue-500 rounded-full transition duration-150"
+								type="submit">Send message</button
+							>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
