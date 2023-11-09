a15afe591597f6a805c07aebbad6c88f9d393601
Andrew <alassetter@skyymedia.com>
2023-11-09 13:41:51 -0600

    Solutions Work

--- src/routes/solutions/+page.svelte
+++ src/routes/solutions/+page.svelte
@@ -49,3 +49,173 @@
 		</div>
 	</div>
 </section>
+
+<!-- Tech Stack -->
+<section class="py-12 md:py-24 bg-coolGray-50">
+	<div class="w-full mx-auto px-12">
+		<div class="max-w-xl lg:max-w-none mx-auto">
+			<div class="flex flex-wrap -mx-4 items-center">
+				<div class="w-full lg:w-5/12 px-4 mb-20 lg:mb-0">
+					<div class="max-w-lg">
+						<span
+							class="inline-block mb-4 py-2 px-3 text-sm text-black group-hover:text-white leading-none border-1.5 border-black group-hover:bg-black rounded-full transition duration-200"
+							>Technology Stack</span
+						>
+						<h2
+							class="font-heading text-6xl sm:text-7xl tracking-tighter mb-10"
+						>
+							Curious How We Stack Up?
+						</h2>
+						<p>
+							As a software development company with years of
+							experience, we only utilize the best in class
+							technologies, tools, and methodologies to provide your
+							company with highly scalable and secure solutions.
+						</p>
+					</div>
+				</div>
+				<div class="w-full lg:w-7/12 px-4">
+					<div class="max-w-xl lg:ml-auto">
+						<div class="max-w-lg">
+							<span class="mr-4">I will become a sponsor</span>
+						</div>
+						<div class="flex flex-wrap -mx-4 -mb-4">
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+							<div class="w-full xs:w-1/2 sm:w-1/3 px-4 mb-4">
+								<div
+									class="flex h-28 p-2 items-center justify-center bg-white rounded-3xl"
+								>
+									<img
+										class="block mx-auto"
+										src="/V-logo.svg"
+										alt=""
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
